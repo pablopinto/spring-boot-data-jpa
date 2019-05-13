fe667dbd4dabceda30b18151d46d48795b24c47a
Pablo Pinto <pabloctender@gmail.com>
2019-05-13 13:45:21 +0200

    Created 1st database with data on view

--- spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,24 @@
 package com.estudiando.springboot.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.estudiando.springboot.app.models.dao.IClienteDao;
+
+@Controller
 public class ClienteController {
 
+	@Autowired
+	private IClienteDao clienteDao;
+
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	public String listar(Model model) {
+		model.addAttribute("titulo", "Listado de clientes");
+		model.addAttribute("clientes", clienteDao.findAll());
+		return "listar";
+
+	}
 }
---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/*Populate tables */
+INSERT INTO clientes (id , nombre , apellido , email , create_at) VALUES(1,'Pablo','Pinto','pabloctender@gmail.com','2019-05-13');
+INSERT INTO clientes (id , nombre , apellido , email , create_at) VALUES(2,'Francisco','Cebrián','pablocod8@gmail.com','2019-05-13');
