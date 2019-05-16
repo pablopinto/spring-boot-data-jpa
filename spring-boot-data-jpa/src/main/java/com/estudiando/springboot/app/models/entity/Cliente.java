6098fd75c8c8fdac67a9e673efa015cfa67116e7
Pablo Pinto <pabloctender@gmail.com>
2019-05-16 11:16:19 +0200

    Last Vid 56

--- spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/models/entity/Cliente.java
@@ -14,7 +14,6 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 //import org.springframework.format.annotation.DateTimeFormat;
 
