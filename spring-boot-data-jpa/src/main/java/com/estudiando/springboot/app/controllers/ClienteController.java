eef1afb54b17ae0e0c4ceb5e8ddf86adc57b3acf
Pablo Pinto <pabloctender@gmail.com>
2019-05-17 12:07:28 +0200

    Images/file upload in an external folder of the project

--- spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/controllers/ClienteController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,6 +35,22 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
+	
+	@GetMapping(value="/ver/{id}")
+	public String ver(@PathVariable(value="id") Long id , Map<String , Object> model , RedirectAttributes flash) {
+		
+		Cliente cliente = clienteService.findOne(id);
+		if(cliente==null) {
+			flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+			return "redirect:/listar";
+		}
+		
+		model.put("cliente", cliente);
+		model.put("titulo" , "Detalles del cliente " + cliente.getNombre());
+		
+		return "ver";
+		
+	}
 
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(@RequestParam(name="page" , defaultValue = "0") int page , Model model) {
@@ -70,8 +87,12 @@ public class ClienteController {
 		}
 		
 		if(!foto.isEmpty()) {
-			Path  directorioRecursos = Paths.get("src//main//resources//static//upload");
-			String rootPath = directorioRecursos.toFile().getAbsolutePath();
+			
+//			Path  directorioRecursos = Paths.get("src//main//resources//static//upload"); Ubicacion de guardado de imagenes en ruta local del proyecto
+//			String rootPath = directorioRecursos.toFile().getAbsolutePath();
+			
+			String rootPath = "C://Temp//uploads"; // Ruta externa al proyecto
+			
 			try {
 				byte[] bytes = foto.getBytes();
 				Path rutaCompleta = Paths.get(rootPath +"//" + foto.getOriginalFilename());
