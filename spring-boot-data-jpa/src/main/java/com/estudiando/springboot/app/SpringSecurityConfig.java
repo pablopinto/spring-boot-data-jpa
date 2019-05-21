62e9cf75088a95cec9a7dbd714004e8672a0f0cf
Pablo Pinto <pabloctender@gmail.com>
2019-05-21 14:59:19 +0200

    Spring Security Config , 1st creation on login and 2 users with rols

---
+++ spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.estudiando.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
+		
+		PasswordEncoder encoder = passwordEncoder();
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
+		builder.inMemoryAuthentication()
+		.withUser(users.username("admin").password("admin").roles("ADMIN" , "USER"))
+		.withUser(users.username("pablo").password("pablo").roles("USER"));
+	}
+	
+}
