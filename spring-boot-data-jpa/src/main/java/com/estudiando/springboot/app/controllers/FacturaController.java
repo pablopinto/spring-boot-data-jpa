9d24657d996878f8f2a3f31e7f38660383c5c86c
Pablo Pinto <pabloctender@gmail.com>
2019-05-21 13:31:25 +0200

    Deleting a bill

--- spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/controllers/FacturaController.java
+++ spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/controllers/FacturaController.java
@@ -114,5 +114,20 @@ public class FacturaController {
 
 		return "redirect:/ver/" + factura.getCliente().getId();
 	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable(value="id") Long id , RedirectAttributes flash) {
+		
+		Factura factura = clienteService.findFacturaById(id);
+		
+		if(factura  != null) {
+			clienteService.deleteFactura(id);
+			flash.addFlashAttribute("success", "Factura eliminada satisfactoriamente");
+			return "redirect:/ver/" + factura.getCliente().getId();
+		}
+		
+		flash.addAttribute("error", "La factura no existe en la base de datos , no se pudo eliminar");
+		return "redirect:/listar";
+	}
 
 }
