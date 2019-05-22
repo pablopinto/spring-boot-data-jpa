42ce4ed4936f56ead33954cea34ae2833ebbbdc2
Pablo Pinto <pabloctender@gmail.com>
2019-05-22 11:44:49 +0200

    MVC config for database security

--- spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/SpringBootDataJpaApplication.java
+++ spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/SpringBootDataJpaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.estudiando.springboot.app.models.service.IUploadFileService;
 
@@ -12,6 +13,10 @@ public class SpringBootDataJpaApplication implements CommandLineRunner {
 	
 	@Autowired
 	IUploadFileService uploadFileService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
@@ -20,7 +25,14 @@ public class SpringBootDataJpaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		uploadFileService.deleteAll();
-		uploadFileService.init();		
+		uploadFileService.init();
+		
+		String password="admin";
+		
+		for(int i = 0 ; i<2 ; i++) {
+			String bcryptPassword = passwordEncoder.encode(password);
+			System.out.println(bcryptPassword);
+		}
 	}
 
 }
--- spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/SpringSecurityConfig.java
+++ spring-boot-data-jpa/src/main/java/com/estudiando/springboot/app/SpringSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.estudiando.springboot.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -17,14 +16,12 @@ import com.estudiando.springboot.app.auth.handler.LoginSuccessHandler;
 @EnableGlobalMethodSecurity(securedEnabled=true , prePostEnabled=true)
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
 	
 	@Autowired
 	private LoginSuccessHandler successHandler;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -51,7 +48,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
 		
-		PasswordEncoder encoder = passwordEncoder();
+		PasswordEncoder encoder = passwordEncoder;
 		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
 		
 		builder.inMemoryAuthentication()
